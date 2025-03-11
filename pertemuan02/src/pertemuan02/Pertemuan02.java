7a5a2c5807dd91e334ed45cbc2e5b1ce66388a56
ayuusawitrii <sawitridenpasar@gmail.com>
2025-03-11 21:55:10 +0800

    Pertemuan02

---
+++ pertemuan02/src/pertemuan02/Pertemuan02.java
@@ -0,0 +1,24 @@
+package pertemuan02;
+
+/**
+ *
+ * @author ayu sawitri
+ * tgl : 11 maret 2025
+ */
+public class Pertemuan02 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String nama = "Ayu Sawitri";
+        String nim = "2301010307";
+        
+        System.out.println("Pertemuan 2 Java");
+        System.out.println("~~~~~~~~~~~~~~~~");
+        System.out.println("Nama saya : "+ nama);
+        System.out.println("Nim saya : "+ nim);
+        System.out.printf("Nama Saya %s dengan Nim %s, saat ini sedang belajar pemrograman bahasa Java", nama,nim);
+    }
+    
+}
